3ab6ddb049cf1397c56b9d013e2c4d10f7f3e7b8
Yuri Shkrobut <georgebest2409@gmail.com>
2023-09-01 14:36:01 +0300

    Component with props

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Counter } from "./Counter";
+
+export const App = () => {
+  const [history, setHistory] = useState<number[]>([]);
+  const [count, setCount] = useState(0);
+  const [query, setQuery] = useState('');
+
+  function saveCount(value: number) {
+    setCount(value);
+    setHistory(currentHistory => [...currentHistory, value]);
+  }
+
+  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value)
+  }
+
+  return (
+    <div className="App">
+      <h1>Mate academy {count} </h1>
+
+      <div className="box">
+        <input
+          type="text"
+          onChange={handleQueryChange}
+        />
+
+        {query}
+      </div>
+
+      <Counter
+        value={count}
+        onChange={saveCount}
+      />
+
+      <div className="box">
+        {history.join(', ') || 'No history yet'}
+      </div>
+    </div>
+  );
+};
