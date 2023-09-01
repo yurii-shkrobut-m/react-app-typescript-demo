5a137a46c296a4eac1c7a8a56e91fae6de00593a
Yuri Shkrobut <georgebest2409@gmail.com>
2023-09-01 17:30:36 +0300

    Types for Sorting Function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.scss';
 
 import postsFromServer from './api/posts.json';
+// import initialPosts from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList';
 import { User } from './types/User';
 import { Comment } from './types/Comment';
 import { Post } from './types/Post';
+import { useState } from 'react';
 
 function getUserById(userId: number): User | null {
   return usersFromServer.find(user => user.id === userId)
@@ -18,17 +20,30 @@ function getCommentById(id: number): Comment[] {
     || null;
 }
 
-const posts: Post[] = postsFromServer.map(post => ({
+const initialPosts: Post[] = postsFromServer.map(post => ({
   ...post,
   user: getUserById(post.userId),
   comments: getCommentById(post.id),
 }));
 
-export const App = () => (
-  <section className="App">
-    <h1 className="App__title">Static list of posts</h1>
+function getPreparedPosts(posts: Post[]) {
+  let preparedPosts = [...posts]
 
-    <PostList posts={posts} />
+  return preparedPosts
+}
 
-  </section>
-);
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [sortField, setSortField] = useState('');
+
+  const visiblePosts = getPreparedPosts(initialPosts)
+
+  return (
+    <section className="App">
+      <h1 className="App__title">Static list of posts</h1>
+
+      <PostList posts={visiblePosts} />
+
+    </section>
+  )
+};
